3e5af3d760c23d43c42e2df278149436d4e2432b
arrgn <cemas045@vk.com>
2022-10-26 22:48:17 +0300

    Move windows to different classes

--- src/main.py
+++ src/main.py
@@ -1,53 +1,17 @@
-import numpy as np
-from PyQt5 import Qt, uic, QtCore
-import pyqtgraph as pg
+from PyQt5 import Qt
+from PyQt5.QtWidgets import QStackedWidget
+from ballistic import BallisticWindow
 
 
 class Window:
     def __init__(self):
         super().__init__()
-        self.ui = Qt.QWidget()
-
-        self.deg2rad = 0.01745329251
+        self.ui = QStackedWidget()
 
         self.init_ui()
 
     def init_ui(self):
-        uic.loadUi("Ballistics.ui", self.ui)
-
-        self.ui.AngleInput.setText("30")
-        self.ui.VelocityInput.setText("10")
-        self.ui.GInput.setText("9.81")
-
-        self.ui.AngleInput.textChanged.connect(self.build_plot)
-        self.ui.VelocityInput.textChanged.connect(self.build_plot)
-        self.ui.GInput.textChanged.connect(self.build_plot)
-
-        self.ui.Plot.setAspectLocked()
-        self.ui.Plot.setLimits(xMin=0, yMin=0)
-
-        self.build_plot()
-
-    def build_plot(self):
-        angle = float(self.ui.AngleInput.text()) * self.deg2rad
-        velocity = float(self.ui.VelocityInput.text())
-        g = float(self.ui.GInput.text())
-
-        x = np.linspace(0, velocity ** 2 * np.sin(2 * angle) / g, 1000)
-        y = self.f(x, angle, velocity, g)
-
-        self.ui.Plot.clear()
-        self.ui.Plot.plot(x=x, y= y,pen=pg.mkPen('w', width=5, style=QtCore.Qt.DashLine))
-
-        try:
-            self.ui.Plot.setYRange(0, np.max(y) * 1.5)
-            self.ui.Plot.setXRange(0, np.max(y) * 1.5)
-        except:
-            pass
-
-    def f(self, x, angle, velocity, g):
-        t = x / (velocity * np.cos(angle))
-        return velocity * np.sin(angle) * t - g * (t ** 2) / 2
+        self.ui.addWidget(BallisticWindow().ui)
 
     def show(self):
         self.ui.show()
