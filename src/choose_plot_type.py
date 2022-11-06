92e3e7879ab24669de653c984ec8ac78b9a6ea24
NumPix <lenchik221006@gmail.com>
2022-11-06 17:45:11 +0300

    Added function input

--- src/choose_plot_type.py
+++ src/choose_plot_type.py
@@ -11,6 +11,8 @@ class ChoosePlotWindow:
             self.vel = QRadioButton()
             self.space = QRadioButton()
 
+            self.functions: [[str, str, str], ...] = []
+
             self.inp_result = 0
 
             self.init_ui()
@@ -24,11 +26,57 @@ class ChoosePlotWindow:
             self.vel.clicked.connect(lambda: self.change_result(2))
             self.space.clicked.connect(lambda: self.change_result(3))
 
+            self.addButton.clicked.connect(lambda: self.list_add_item())
+            self.removeButton.clicked.connect(lambda: self.list_remove_item())
+
+            self.leftBorderInput.textChanged.connect(lambda: self.autosave())
+            self.rightBorderInput.textChanged.connect(lambda: self.autosave())
+            self.functionInput.textChanged.connect(lambda: self.autosave())
+
+            self.listWidget.currentRowChanged.connect(lambda: self.list_on_row_changed())
+
             self.accel.click()
+            self.list_add_item()
+
+        def autosave(self):
+            if self.sender().objectName() == "leftBorderInput":
+                change_index = 0
+            elif self.sender().objectName() == "rightBorderInput":
+                change_index = 1
+            else:
+                change_index = 2
+
+            self.functions[self.listWidget.currentRow()][change_index] = self.sender().text()
 
         def change_result(self, new_result: int):
             self.inp_result = new_result
 
+        def list_add_item(self):
+
+            self.listWidget.addItem(f"el_{self.listWidget.count()}")
+            self.functions.append(["0", "0", ""])
+
+            self.leftBorderInput.setText("0")
+            self.rightBorderInput.setText("0")
+
+            self.functionInput.setText("")
+
+        def list_remove_item(self):
+
+            if len(self.functions) <= 1:
+                return
+
+            selected_row = self.listWidget.currentRow()
+
+            self.listWidget.takeItem(selected_row)
+            self.functions.pop(selected_row)
+
+        def list_on_row_changed(self):
+            selected_row = self.listWidget.currentRow()
+
+            self.leftBorderInput.setText(self.functions[selected_row][0])
+            self.rightBorderInput.setText(self.functions[selected_row][1])
+            self.functionInput.setText(self.functions[selected_row][2])
 
     def __init__(self):
         super().__init__()
