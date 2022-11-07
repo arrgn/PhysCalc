422d80197eab829fefe901a0f5d52f5170b915dc
arrgn <cemas045@vk.com>
2022-11-07 20:45:20 +0300

    Refactor *.ui file names

--- src/main.py
+++ src/main.py
@@ -25,7 +25,7 @@ class Window(QWidget):
         self.ui.addWidget(CalcsWindow().ui)
 
         self.drop_menu = QFrame(self)
-        uic.loadUi("Drop_menu.ui", self.drop_menu)
+        uic.loadUi("drop_menu.ui", self.drop_menu)
 
         self.drop_menu.w1_btn.clicked.connect(self.switch_window)
         self.drop_menu.w2_btn.clicked.connect(self.switch_window)
